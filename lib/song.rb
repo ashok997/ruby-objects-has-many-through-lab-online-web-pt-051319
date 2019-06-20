8f20621211a58c18dbed4aaf803abdfa893f8c4f
ASHOK MAHARJAN <ashok997@gmail.com>
2019-06-20 23:28:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,5 +17,19 @@ class Song
   
 end
 =======
-class Song     attr_accessor :name, :genre, :artist    @@all = []        def initialize(name, artist, genre)      @name = name      @genre = genre      @artist = artist      @@all << self    end        def self.all      @@all    end end
+class Song 
+    attr_accessor :name, :genre, :artist
+    @@all = []
+    
+    def initialize(name, artist, genre)
+      @name = name
+      @genre = genre
+      @artist = artist
+      @@all << self
+    end
+    
+    def self.all
+      @@all
+    end 
+end
 >>>>>>> 018d1e66c5008ee61d60596ad9dc05ccd91c7310
